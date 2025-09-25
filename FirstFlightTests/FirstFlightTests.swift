6ca74233d506903fc24675b91a64de7e8169604a
Yurii Voievodin <voevodin.yura@icloud.com>
2025-09-25 17:49:53 +0200

    Initial Commit

---
+++ FirstFlightTests/FirstFlightTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstFlightTests.swift
+//  FirstFlightTests
+//
+//  Created by Yurii Voievodin on 25/09/2025.
+//
+
+import Testing
+@testable import FirstFlight
+
+struct FirstFlightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
